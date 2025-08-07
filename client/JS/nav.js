ebf95ee5780f7bdd20ff8df6b46e028394f2dd65
mikaaran <mikaaran@gmail.com>
2025-08-07 22:36:19 +0300

    updated nav not finish(admin)

---
+++ client/JS/nav.js
@@ -0,0 +1,6 @@
+﻿document.addEventListener('DOMContentLoaded', () => {
+    const nav = document.getElementById('mainNav');
+    const btn = nav.querySelector('.hamburger');
+    if (!btn) return;
+    btn.addEventListener('click', () => nav.classList.toggle('open'));
+});
